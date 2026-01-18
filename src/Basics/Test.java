708e20341389f5f100ae4963c9d5a2c15977f655
Udityaduvanshi <udity085@gmail.com>
2026-01-18 18:53:58 +0530

    sixth commt

--- src/Basics/Test.java
+++ src/Basics/Test.java
@@ -1,86 +1,29 @@
 package Basics;
 
 public class Test {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
 
 
+        String Correctusername = "admin22";
+        String Correctpassword = "hi";
 
-         // atm
-/*
-        int correctpin = 4621;
-        double balance = 12000;
         // input
-        int pinnumber = 4621;
-        double withdrawal =1300;
 
-        if (pinnumber == correctpin && withdrawal > 0) {
-            if (withdrawal <= balance) {
-                System.out.println("Pin Number : " + pinnumber);
-                System.out.println("Balance : " + balance);
-                System.out.println("Withrawing Amount  : " + withdrawal);
-                System.out.println("Remaining Balance  : " + (balance - withdrawal));
+        String username = "admin22";
+        String password = "h";
+
+        if (username.equals(Correctusername)) {
+            if (password.equals(Correctpassword)) {
+                System.out.println("Login Successfull");
             } else {
-                System.out.println("Insufficient balance");
+                System.out.println("Wrong Password");
             }
-        } if ( pinnumber == correctpin && withdrawal == 0) {
-            System.out.println("Pin is correct");
-            System.out.println("Invalid amount number");
-        }
-        if  (pinnumber != correctpin) {
-            System.out.println("Invalid Pin");
-        }*/
-
-
-
-         // salary slip
-
-        /* double salary = 12000;
-         double HRA, DA, GrossSalary;
-
-         if (salary <= 10000) {
-             HRA = salary * 0.10;
-             DA = salary * 0.90;
-         } else {
-             HRA = salary * 0.20;
-             DA = salary * 0.95;
-         }
-
-         double totalGross = (salary + HRA + DA);
-
-
-         System.out.println("Salary : " + salary);
-         System.out.println("HRA : " + HRA);
-         System.out.println("DA : " + DA);
-         System.out.println("Total GrossSalary : " + totalGross);*/
-
-
-
-
-         // Department Bonus
-
-        int salary = 100000;
-        String department = "tranie";
-        double bonus;
-
-        if (department.equalsIgnoreCase("HR")) {
-            bonus = salary * 0.10;
-        } else if (department.equalsIgnoreCase("IT")) {
-            bonus = salary * 0.20;
-        }else if (department.equalsIgnoreCase("SALES")) {
-            bonus = salary * 0.25;
         } else {
-             bonus = salary * 0.05;
+            System.out.println("Invalid username");
         }
 
-        double totalGross = (salary + bonus);
 
-        System.out.println("Department : " + department);
-        System.out.println("Salary : " + salary);
-        System.out.println("Bonus : " + bonus);
-        System.out.println("Total Gross : " + totalGross);
-
-
-
+        //
 
 
     }
