49f02e501a72ffcac4bb880f5e84739d3b048355
Udityaduvanshi <udity085@gmail.com>
2026-01-18 14:10:57 +0530

    second comment

--- src/Basics/Test.java
+++ src/Basics/Test.java
@@ -24,5 +24,67 @@ public class Test {
         }
 
         System.out.println("Total electricity bill: " + bill);
+
+         // Exam result
+
+        int sub1 = 32;
+        int sub2 = 39;
+        int sub3 = 48;
+        int sub4 = 54;
+        int sub5 = 63;
+        boolean fail = false;
+
+        if (sub1 >= 33) {
+            System.out.println("pass in sub1 : " + sub1);
+        } if (sub1 <= 33) {
+            System.out.println("fail in sub1 : " + sub1);
+            fail = true;
+        }if (sub2 >= 33) {
+            System.out.println("pass in sub2 : " + sub2);
+        } if (sub2 <= 33) {
+            System.out.println("fail in sub2 : " + sub2);
+            fail = true;
+        }if (sub3 >= 33) {
+            System.out.println("pass in sub3 : " + sub3);
+        } if (sub3 <= 33) {
+            System.out.println("fail in sub3 : " + sub3);
+            fail = true;
+        }if (sub4 >= 33) {
+            System.out.println("pass in sub4 : " + sub4);
+        } if (sub4 <= 33) {
+            System.out.println("fail in sub4 : " + sub4);
+            fail = true;
+        }if (sub5 >= 33) {
+            System.out.println("pass in sub5 : " + sub5);
+        } if (sub5 <= 33) {
+            System.out.println("fail in sub5 : " + sub5);
+            fail = true;
+        }
+
+        int percentage = (sub1+sub2+sub3+sub4+sub5)/ 5;
+        System.out.println("Percentage : " + percentage + "%");
+
+         if (!fail) {
+
+             if (percentage >= 60) {
+                 System.out.println("First division");
+             } else if (percentage >= 45) {
+                 System.out.println("Second division");
+             } else if (percentage >= 33) {
+                 System.out.println("Third division");
+             } else {
+                 System.out.println("fail");
+             }
+         }
+
+        if (fail) {
+            System.out.println("Result : Fail");
+        } else {
+            System.out.println("Result : Pass");
+        }
+
+
+
+
     }
 }
