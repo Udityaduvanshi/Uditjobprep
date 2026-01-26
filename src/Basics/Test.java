1bda3a6d07206068495db0e1d704345ad8aa50a0
Udityaduvanshi <udity085@gmail.com>
2026-01-26 16:08:25 +0530

    loop hard que

--- src/Basics/Test.java
+++ src/Basics/Test.java
@@ -5,130 +5,187 @@ public class Test {
 
 
 
-        // for loop
+        // hard
+        // armstrong
 
-        // factorial of a no
-        int fact = 1;
-        for (int i = 1; i <= 5; i++) {
-            fact = fact * i;
-            System.out.println(i + "*" + i + ":" + fact);
+        int numb = 153;
+        int original = numb;
+        int sum = 0;
+        while (numb > 0) {
+            int remai = numb % 10;
+            sum = sum + (remai * remai * remai);
+            numb = numb / 10;
         }
-
-        // check num is prime
-
-      /*  int no1= 7;
-        for (int i = 2; i <= no1-1; i++){
-            if (no1 % 2 == 0){
-                System.out.println("Is Not prime");
-                return;
-            }else {
-                System.out.println("Is Prime");
-                return;
-            }
-        }*/
-
-        // num is prime
-        int no2 = 7;
-        int temp = 0;
-        for (int i = 2; i<= no2-1; i++){
-            if (no2 % 2 == 0){
-                temp++;
-            }
-        }
-        if (temp == 0){
-            System.out.println("Number is Prime");
+        if (sum == original) {
+            System.out.println("armstrong number");
         } else {
-            System.out.println(" Number Is not Prime");
+            System.out.println(" is not armstrong");
         }
 
-        // all prime nub 1 to 100
-        for (int num = 1; num <= 100;num++) {
-            int temp1 = 0;
-            for (int i = 2; i <= num - 1; i++) {
-                if (num % i == 0) {
-                    temp1++;
-                }
+        // 1 to n
+        for (int i = 1; i<= 1000; i++){
+            int num = i;
+            int sum1 = 0;
+
+            while (num > 0) {
+                int remai = num % 10;
+                sum1 = sum1 + (remai * remai * remai);
+                num = num / 10;
             }
-            if (temp1 == 0) {
-                System.out.println(num);
+            if (sum1 == i) {
+                System.out.println( i + " is armstrong number");
+            }
+
+        }
+
+
+
+        // 5 pattern
+        int n = 4;
+        for ( int i = 1; i <= n; i++){
+            // spaces
+            for( int s = n ; s > i ;s--){
+                System.out.print("  ");
+            }
+            // star
+            for (int j=1;j<=( 2*i -1);j++){
+                System.out.print(" *");
+            }
+
+            System.out.println();
+        }
+
+        // num pyramid
+
+        int n1 = 4;
+        for ( int i = 1; i <= n1; i++){
+            // spaces
+            for( int s = n1 ; s > i ;s--){
+                System.out.print("  ");
+            }
+            // star
+            for (int j=1;j<=i;j++){
+                System.out.print(j + " ");
+            }
+            // other side
+            for (int j=i -1;j>=1;j--){
+                System.out.print( j + " ");
+            }
+
+            System.out.println();
+        }
+
+        // perfect
+        int n3 = 6;
+        int sum2 =0;
+        for (int i = 1; i < n3;i++ ){
+            if ( n3 % i == 0){
+                sum2 = sum2 + i;
             }
         }
-        // reverse
-        int number1= 12152;
-        int reverse1 = 0;
-
-        for (int i = number1; i!=0; i/=10){
-            reverse1 = reverse1 *10 + i % 10;
-        }
-        System.out.println("reverse  a number " + reverse1);
-
-        // palindrome
-        int number= 121;
-        int original = number;
-        int reverse = 0;
-
-        for (int i = number; i!=0; i/=10){
-            reverse = reverse *10 + i % 10;
-        }
-        if (original == reverse){
-            System.out.println( number + " is a palindrome number");
+        if (sum2 == n3){
+            System.out.println(sum2 + " is a perfect number ");
         } else {
-            System.out.println("is not a palindrome number");
-        }
-
-        // sum of digits of a num
-        int numb2 = 121565;
-        int sum2 = 0;
-        for (; numb2 >0; numb2  /= 10){
-            int digit = numb2 % 10;
-            sum2 = sum2 + digit;
-        }
-        System.out.println("sum of digits is " + sum2);
-
-        // fabonacci
-
-        int a =0;
-        int b = 1;
-        for (int i = 1;i<=10;i++){
-            System.out.print(a + " ");
-            int c = a+b;
-            a = b;
-            b = c;
+            System.out.println(sum2 + "is not a perfect number");
         }
 
 
+        // decimal to binary
 
-        // largest number  ina number
-          int numb = 121565;
-          int largest = 0;
-          for (; numb >0; numb  /= 10){
-              int digit = numb % 10;
-                      if (digit >largest){
-                          largest = digit;
-                      }
-          }
-        System.out.println( "largest no in a number  " + largest);
+        int decimal = 25;
+        int binary = 0;
+        int pos = 1;
 
-          // frequency
+        for( ; decimal>0;decimal/=2){
+            int rem = decimal % 2;
+            binary = binary + rem * pos ;
+            pos = pos * 10;
+        }
+        System.out.println("binary is : " + binary);
 
-        int numb4 = 121565;
-        int digitCount1 = 5;
-        int count1 =0 ;
-        for (; numb4 >0; numb4  /= 10){
-            int digit = numb4 % 10;
-            if ( digit == digitCount1){
-                count1++;
+        // strong number
+
+        int number = 145;
+        int oringal1 = number;
+        int sum3 = 0;
+
+        while (number > 0 ){
+            int remain = number % 10;
+            // factorial
+            int fact = 1;
+
+            for (int i= 1; i<= remain; i++){
+                fact = fact * i;
             }
-        }
-        System.out.println("frequency of " + digitCount1 + " : " + count1);
 
-        // pattern
-        for (int i = 0; i<5;i++){
-            for (int j =0;j <=i;j++){
-                System.out.print("*");
+            sum3 = sum3 + fact ;
+            number = number / 10;
+        }
+        if (sum3 == oringal1){
+            System.out.println(oringal1 + "  is a Strong number");
+        } else {
+            System.out.println(oringal1 + " is not strong number");
+        }
+
+        // pascal triangle
+
+        int n5 = 5;
+        for (int i = 0; i < n5; i++){
+
+
+            int num = 1;
+
+            for (int j = 0; j<=i; j++){
+                System.out.print( num + " ");
+                num = num * (i - j) / (j + 1);
             }
             System.out.println();
         }
+
+        // lcm
+      /*  int a = 12;
+        int b = 18;
+
+        int max =  (a>b) ? a : b;
+
+         while (true) {
+             if( max % a ==0 && max % b == 0){
+                 System.out.println("lcm = " + max);
+                 break;
+             }
+             max++;
+         }*/
+
+        //
+        int a = 12;
+        int b = 18;
+        int lcm = 0;
+
+        int max =  (a>b) ? a : b;
+
+        for (int i = max; ; i++) {
+            if (i % a == 0 && i % b == 0) {
+                lcm = i;
+                break;
+            }
+        }
+        System.out.println("lcm = " + lcm);
+
+        // hcf
+
+        int no1 = 56;
+        int no2 = 98;
+        int gcd = 1;
+
+        int min =  (no1 < no2) ? no1 : no2;
+
+        for (int i = min;i >=1 ; i--) {
+            if (no1 % i == 0 && no2 % i == 0) {
+                gcd = i;
+                break;
+            }
+        }
+        System.out.println("hcf of " + no1 + " and " + no2 + " is " + gcd);
 
 
 
